e5f55381755e776e6aa4707eecf5db5f66e769f4
Victor Jian Choe <vchoe89@gmail.com>
2019-04-14 14:24:47 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,10 +1,7 @@
 def using_until
   levitation_force = 6
-<<<<<<< HEAD
 until levitation_force == 10
-=======
-until levitation_force = 10
->>>>>>> 7a3e966b7dc7c6029fa789ffa7db3fe38c749c26
+
   puts("Wingardium Leviosa")
   levitation_force = levitation_force + 1
   end
